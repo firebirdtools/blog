fc652f39a55d3e4dc4a7e9105d738cfbbf8adfc3
firebirdtools <hexiaobinsx@126.com>
2019-01-18 13:16:08 +0800

    add COLLAPSE_FIRST_ARTICLE

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,12 +3,13 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'firebird'
-SITENAME = 'blog'
+SITENAME = 'Blog'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+# TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Shanghai'	
 
 DEFAULT_LANG = 'en'
 
@@ -33,3 +34,76 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+
+# ============================================================================
+
+# =============================================================================
+# add m.css-theme
+THEME = 'm.css/pelican-theme'
+THEME_STATIC_DIR = 'static'
+DIRECT_TEMPLATES = ['index']
+
+M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600',
+                'static/m-dark.css']
+
+M_THEME_COLOR = '#22272e'
+
+PLUGIN_PATHS = ['m.css/pelican-plugins']
+PLUGINS = ['m.htmlsanity']
+
+
+
+PLUGINS = ['m.htmlsanity', 'm.images']
+M_IMAGES_REQUIRE_ALT_TEXT = False
+STATIC_PATHS = ['static']
+
+
+
+PLUGINS += ['m.htmlsanity', 'm.math']
+M_MATH_RENDER_AS_CODE = False
+M_MATH_CACHE_FILE = 'm.math.cache'
+
+
+
+PLUGINS += ['m.htmlsanity', 'm.components']
+
+PLUGINS += ['m.htmlsanity', 'm.code']
+
+
+
+
+# =============================================================================
+AUTHOR = 'firebird'
+M_SITE_LOGO_TEXT = 'Blog'
+SITENAME = 'Blog'
+
+#PLUGINS = ['m.abbr',
+#          'm.components',
+#           'm.dox',
+#           'm.dot',
+#           'm.filesize',
+#           'm.gl',
+#           'm.gh',
+#           'm.htmlsanity',
+#           'm.images',
+#           'm.link',
+#           'm.math',
+#           'm.metadata',
+#           'm.plots',
+#           'm.vk']
+
+
+
+# =============================================================================
+M_FAVICON = ('static/firebird.png', 'image/x-icon')
+M_LINKS_NAVBAR1 = [('FBT', 'https://www.firebirdtool.com/', '', []),                   
+                   ('Docs', 'https://www.firebirdtool.com/doc-cpplogging/', '', [
+                        ])]
+
+
+# =============================================================================
+
+# 博客中第一个文件展开设置
+M_COLLAPSE_FIRST_ARTICLE = True
--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://firebirdtools.github.io/blog'
 
 RELATIVE_URLS = False
 
@@ -24,70 +24,3 @@ DELETE_OUTPUT_DIRECTORY = True
 
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
-
-
-# =============================================================================
-# add m.css-theme
-THEME = 'm.css/pelican-theme'
-THEME_STATIC_DIR = 'static'
-DIRECT_TEMPLATES = ['index']
-
-M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600',
-                'static/m-dark.css']
-
-M_THEME_COLOR = '#22272e'
-
-PLUGIN_PATHS = ['m.css/pelican-plugins']
-PLUGINS = ['m.htmlsanity']
-
-
-
-PLUGINS = ['m.htmlsanity', 'm.images']
-M_IMAGES_REQUIRE_ALT_TEXT = False
-STATIC_PATHS = ['static']
-
-
-
-PLUGINS += ['m.htmlsanity', 'm.math']
-M_MATH_RENDER_AS_CODE = False
-M_MATH_CACHE_FILE = 'm.math.cache'
-
-
-
-PLUGINS += ['m.htmlsanity', 'm.components']
-
-PLUGINS += ['m.htmlsanity', 'm.code']
-
-
-
-
-# =============================================================================
-AUTHOR = 'firebird'
-M_SITE_LOGO_TEXT = 'blog'
-SITENAME = 'blog'
-
-#PLUGINS = ['m.abbr',
-#          'm.components',
-#           'm.dox',
-#           'm.dot',
-#           'm.filesize',
-#           'm.gl',
-#           'm.gh',
-#           'm.htmlsanity',
-#           'm.images',
-#           'm.link',
-#           'm.math',
-#           'm.metadata',
-#           'm.plots',
-#           'm.vk']
-
-
-
-# =============================================================================
-M_FAVICON = ('static/firebird.png', 'image/x-icon')
-M_LINKS_NAVBAR1 = [('FBT', 'https://www.firebirdtool.com/', '', []),                   
-                   ('Docs', 'https://www.firebirdtool.com/doc-cpplogging/', '', [
-                        ])]
-
-
-SITEURL = 'https://firebirdtools.github.io/blog'
